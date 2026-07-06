ac41ceb67537f3564d36aed0c572d55cd9930874
Shresth Raj <contact.shresthraj@gmail.com>
2026-07-06 12:51:00 +0530

    chore: upgrade share_plus to v13, flutter_secure_storage to v10, fix KGP warnings

--- swarnayan_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ swarnayan_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,7 @@ import Foundation
 
 import app_links
 import file_selector_macos
-import flutter_secure_storage_macos
+import flutter_secure_storage_darwin
 import printing
 import share_plus
 import shared_preferences_foundation
@@ -17,7 +17,7 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
